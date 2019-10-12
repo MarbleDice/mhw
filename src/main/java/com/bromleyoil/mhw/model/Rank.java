17ec1b13aa3b03bb18d6791e5f660aadf74082a1
MarbleDice <nagasone@gmail.com>
2019-10-11 20:30:05 -0700

    Add Rank enum

---
+++ src/main/java/com/bromleyoil/mhw/model/Rank.java
@@ -0,0 +1,6 @@
+package com.bromleyoil.mhw.model;
+
+public enum Rank {
+	HR1, HR2, HR3, HR4, HR5, HR6, HR7, HR8, HR9, HR10, HR11, HR12, HR13, HR14, HR15, HR16, HR30, HR50, HR60, HR100,
+	MR1, MR2, MR3, MR4, MR5, MR6, MR7, MR8, MR9, MR10, MR11, MR12, MR13, MR14, MR15, MR16, MR17, MR50, MR70, MR100;
+}
